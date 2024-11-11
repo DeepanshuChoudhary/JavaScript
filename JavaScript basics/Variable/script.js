a878452c9f487934cbe2db0ee074746ca478aae6
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-11-11 18:13:31 +0530

    JavaScript learning

---
+++ JavaScript basics/Variable/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// let a = 5;
+
+// let a = "Hello";
+
+let myName = "Deepanshu Choudhary";
+let a = myName;
+console.log(a);
+
+var name = "Nishant Nagar";
+var name = "Aakash Kasana";
+console.log(name);
