78e3190c445c68ba08fb3fb38dda5a21e51005a9
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-11-12 10:26:34 +0530

    JavaScript learning

---
+++ JavaScript basics/forEach function Array/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const num = [1,2,3,4,5,6,7,8,9,10];
+
+// num.forEach(num => console.log(num)); 
+
+// Square value
+
+num.forEach((num) => {
+    let square = num * num;
+    console.log(square);
+})
+
+console.log(num);
