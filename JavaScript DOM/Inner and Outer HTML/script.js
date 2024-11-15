d11788731f2cbf8bdf1cbaa70ca532aa46fa6cf1
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-11-15 22:49:15 +0530

    JavaScript learning

---
+++ JavaScript DOM/Inner and Outer HTML/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// innerHTML
+// let a = document.querySelector('.hero');
+// // a.innerHTML = '<b>Anshu Choudhary</b>';
+// // a.innerHTML = '<i>Anshu Choudhary</i>';
+// a.innerHTML += '<i> Anshu Choudhary</i>';
+// console.log(a);
+
+// outerHTML
+let a = document.querySelector('.hero');
+// a.outerHTML = '<b>Anshu Choudhary</b>';
+// a.outerHTML = '<i>Anshu Choudhary</i>';
+a.outerHTML += '<i> Anshu Choudhary</i>';
+console.log(a);
