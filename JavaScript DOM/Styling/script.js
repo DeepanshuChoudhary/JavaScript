a775c266ece8d8983db5739d1387d96b7a4456cb
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-11-16 16:52:29 +0530

    JavaScript learning

---
+++ JavaScript DOM/Styling/script.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "red";
+
+let a = document.querySelector('h1');
+a.style.backgroundColor = "white";
+a.style.color = "blue";
+a.style.border = "5px solid green";
+a.style.borderRadius = "10px"
